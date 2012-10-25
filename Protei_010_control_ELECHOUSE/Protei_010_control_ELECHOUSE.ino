a9cded65e3697fd36718861f533e0d17b92b10a8
gabriella levine <gabriellalevine@gabriellas-MacBook-Air.local>
2012-10-25 23:12:11 +0200

    working now tith the elec house motor driver

--- Protei_010_control_ELECHOUSE/Protei_010_control_ELECHOUSE.ino
+++ Protei_010_control_ELECHOUSE/Protei_010_control_ELECHOUSE.ino
@@ -29,29 +29,23 @@ int Thrust;
 int RightMotor;
 int LeftMotor;
 
-const int RX3= 7;
-const int RX1=6;
+const int RX3= 14;
+const int RX1=15;
 
+//import motor library
+#include <MOTOR.h>
 
-const int OUTPUT_1 = 11; // Analog output pin that the Motor1 pwm pin is attached to
-const int DIR_PIN_1=13; //digital output for logic direction pin motor 1
-const int OUTPUT_2= 10; // Analog output pin that the MOTOR2 pwm pin is attached to
-const int DIR_PIN_2 = 12;//digitaloutput for logic direction pin motor2
-const int ledPower = 8;
-
+const int ledPower = 13;
 
 void setup()
 {
+  // motor driver initialize 
+  motor.begin();
 
   Serial.begin(19200);
   Serial.println("Ready");
   pinMode(ledPower, OUTPUT);
   digitalWrite(ledPower, HIGH);
-
-  pinMode(OUTPUT_1,OUTPUT);
-  pinMode(OUTPUT_2,OUTPUT);
-  pinMode(DIR_PIN_1, OUTPUT);
-  pinMode(DIR_PIN_2, OUTPUT);
 
   pinMode (RX3, INPUT); // connect Rx channel 1 to PD7, which is labled "D7" on the Arduino board
   pinMode (RX1, INPUT); // connect Rx channel 2 to PD6, which is labled "D6" on the Arduino board
@@ -67,28 +61,21 @@ void setup()
 void loop()
 {
   Channel2Value = pulseIn (RX1, HIGH, 20000); //read RC channel 2
-//  Serial.print("Initial Throttle: ");
-//  Serial.print (InitialThrottle);
-//  Serial.print("    ");
-//  Serial.print("Channel 2: ");
-  
-    Channel1Value = pulseIn (RX3, HIGH, 20000); //read RC channel 2
-//  Serial.print("Initial Steer: ");
-//  Serial.print (InitialSteer);
-//  Serial.print("    ");
-//  Serial.print("Channel 1: ");
-       Serial.print("   motor_body :    ");
 
-  motorBody();
-   Serial.print("    motor_sail :    ");
+  Channel1Value = pulseIn (RX3, HIGH, 20000); //read RC channel 2
+  Serial.print("   motor_body :    ");
+
+ motorBody();
+  Serial.print("    motor_sail :    ");
 
   motorSail();
-   Serial.println("");
+  Serial.println("");
 
 }
+
 void motorBody(){
-    if(Channel2Value==0){
-    digitalWrite(OUTPUT_1, LOW);
+  if(Channel2Value==0){
+ motor.close(B);
     Serial.print(Channel2Value);
     Serial.print("     no signal   ");
   }
@@ -96,25 +83,28 @@ void motorBody(){
     int motor_body = map(Channel2Value,1020,1875, 0,255);
     Serial.print(motor_body);
     if(motor_body<=100){
-      digitalWrite(DIR_PIN_1, LOW);
       Serial.print("     backwards     ");
-      analogWrite(OUTPUT_1, map(motor_body, 110,0,0,255));
-     
+      motor_body= map(motor_body, 110,0,0,255);
+           motor.set(B, motor_body, REVERSE);
+
     }
     else if(motor_body>=150){
-      digitalWrite(DIR_PIN_1, HIGH);
+    
       Serial.print("     forwards    ");
-      analogWrite(OUTPUT_1, map(motor_body, 150,255,0,255));
+      motor_body= map(motor_body, 150,255,0,255);
+                 motor.set(B, motor_body, FORWARD);
+
     }
     else{
       Serial.print("     STOP    ");
-      digitalWrite(OUTPUT_1, LOW);
+    motor.close(B);
     } 
   }
 }
+
 void motorSail(){
-    if(Channel1Value==0){
-    digitalWrite(OUTPUT_2, LOW);
+  if(Channel1Value==0){
+ motor.close(A);
     Serial.print(Channel1Value);
     Serial.print("   no signal   ");
   }
@@ -122,23 +112,25 @@ void motorSail(){
     int motor_sail = map(Channel1Value,1020,1875, 0,255);
     Serial.print(motor_sail);
     if(motor_sail<=100){
-      digitalWrite(DIR_PIN_2, LOW);
       Serial.print("     backwards     ");
-      analogWrite(OUTPUT_2, map(motor_sail, 110,0,0,255));
+      motor_sail = map(motor_sail, 110,0,0,255);
+           motor.set(A, motor_sail, REVERSE);
+
     }
     else if(motor_sail>=150){
-      digitalWrite(DIR_PIN_2, HIGH);
       Serial.print("     forwards    ");
-      analogWrite(OUTPUT_2, map(motor_sail, 150,255,0,255));
+     motor_sail = map(motor_sail, 150,255,0,255);
+     motor.set(A, motor_sail, FORWARD);
     }
     else{
       Serial.print("     STOP    ");
-      digitalWrite(OUTPUT_2, LOW);
+     motor.close(A);
     } 
   }
 }
 
 //set the limit
+
 
 
 
